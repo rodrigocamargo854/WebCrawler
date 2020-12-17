7edcea3b9721473f2f4cd9ac3bbbcb930ad4ba60
rodrigocamargo <rodrigocamargo854@gmail.com>
2020-12-17 14:57:42 -0300

    Conversao arquivo json

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 
+
+//resultado da megasena com json
 namespace WebCrawler
 {
     class Program
@@ -12,41 +15,51 @@ namespace WebCrawler
             Console.WriteLine("Informe o numero do concurso");
             string numeroConcurso = Console.ReadLine();
 
-            if(string.IsNullOrEmpty (numeroConcurso))
+            if (string.IsNullOrEmpty(numeroConcurso))
             {
-                numeroConcurso = "2103";
+                numeroConcurso = "2102";
             }
 
-            string url = @"http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso=" + numeroConcurso ;
-            string html;
+            string url = @"http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage//p=concurso=" + numeroConcurso;
+            string json;
             //utilizando lib webclient
-            using(WebClient wc = new WebClient())
+            using (WebClient wc = new WebClient())
             {
                 //para validar a segurança
                 wc.Headers["Cookie"] = "security=true";
-                html = wc.DownloadString(url);
+                //download da url (html)
+                json = wc.DownloadString(url);
+
             }
 
-            html = html.Replace("<span class=\"num_sorteio\"><ul>",""); //retira o span
-            html = html.Replace("</ul></span>",""); //retira o span
-            html = html.Replace("</li>",""); //retira o span
+            Resultado resultadoMegasena = JsonConvert.DeserializeObject<Resultado>(json);
+            //limpeza do html
+            // html = html.Replace("<span class=\"num_sorteio\"><ul>",""); //retira o span
+            // html = html.Replace("</ul></span>",""); //retira o span
+            // html = html.Replace("</li>",""); //retira o span
 
-            string[] vet = Regex.Split(html, "<li>");
-            List<int> resultado = new List<int>();
+            // string[] vet = Regex.Split(html, "<li>");
+            // List<int> resultado = new List<int>();
 
-            resultado.Add(int.Parse(vet[1]));
-            resultado.Add(int.Parse(vet[2]));
-            resultado.Add(int.Parse(vet[3]));
-            resultado.Add(int.Parse(vet[4]));
-            resultado.Add(int.Parse(vet[5]));
-            resultado.Add(int.Parse(vet[6].Substring(0,2)));
+            // resultado.Add(int.Parse(vet[1]));
+            // resultado.Add(int.Parse(vet[2]));
+            // resultado.Add(int.Parse(vet[3]));
+            // resultado.Add(int.Parse(vet[4]));
+            // resultado.Add(int.Parse(vet[5]));
+            // resultado.Add(int.Parse(vet[6].Substring(0,2)));
 
+            // foreach (var item in resultado)
+            // {
+            //     Console.WriteLine(item);
+            // }
+            var resultado = resultadoMegasena.listaDezenas;
             foreach (var item in resultado)
             {
                 Console.WriteLine(item);
             }
+           
 
-        
-        }   
+
+        }
     }
 }
---
+++ Resultado.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace WebCrawler
+{
+    public class Resultado
+    {
+        public List<string> listaDezenas { get; set; }
+
+    }
+}
